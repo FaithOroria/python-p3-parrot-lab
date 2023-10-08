0ffebd421f0e6762754398e7306aef13a68937cf
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-10-08 22:02:23 +0300

    Compleded Asignment

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,20 @@
-def parrot():
-    pass
+import io
+import sys
+import unittest
+
+def parrot(argument="Squawk!"):
+    print(argument)
+    return argument
+
+class TestParrot(unittest.TestCase):
+    def test_prints_argument(self):
+        '''prints the argument passed to it.'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        result = parrot("Hello!")  # Call parrot with an argument
+        sys.stdout = sys.__stdout__  # Restore sys.stdout
+        self.assertEqual(captured_out.getvalue(), "Hello!\n")
+        self.assertEqual(result, "Hello!")
+
+if __name__ == '__main__':
+    unittest.main()
